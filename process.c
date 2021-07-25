e4c4e7f9a0a7cc9319c60dd21125edcc7404f24b
Cacodemon345 <wahil1976@outlook.com>
2021-07-25 22:51:48 +0600

    Set current directory of the process that will be launched

--- process.c
+++ process.c
@@ -20,21 +20,25 @@ NTSTATUS CreateNativeProcess(IN PCWSTR file_name, IN PCWSTR cmd_line, OUT PHANDL
  UNICODE_STRING imgpath; // Nt ImagePath
  UNICODE_STRING dllpath; // Nt DllPath (DOS Name)
  UNICODE_STRING cmdline; // Nt CommandLine
+ UNICODE_STRING curdir; // Nt CurrentDirectory.
  PRTL_USER_PROCESS_PARAMETERS processparameters; // ProcessParameters
  RTL_USER_PROCESS_INFORMATION processinformation={0}; // ProcessInformation
  WCHAR Env[2] = { 0,0 }; // Process Envirnoment
  PKUSER_SHARED_DATA SharedData = (PKUSER_SHARED_DATA)USER_SHARED_DATA; // Kernel Shared Data
+ WCHAR CurrentDirectory[MAX_PATH]; // Current Directory.
 
  *hProcess = NULL;
 
  RtlDosPathNameToNtPathName_U(file_name, &nt_file, &file_part, NULL);
+ RtlCliGetCurrentDirectory(CurrentDirectory);
  
  RtlInitUnicodeString(&imgpath, nt_file.Buffer); // Image path
  RtlInitUnicodeString(&imgname, file_part); // Image name
  RtlInitUnicodeString(&dllpath, SharedData->NtSystemRoot); // DLL Path is %SystemRoot%
  RtlInitUnicodeString(&cmdline, cmd_line); // Command Line parameters
+ RtlInitUnicodeString(&curdir, CurrentDirectory); // Current directory
  
- status = RtlCreateProcessParameters(&processparameters, &imgname, &dllpath, &dllpath, &cmdline, Env, 0, 0, 0, 0);
+ status = RtlCreateProcessParameters(&processparameters, &imgname, &dllpath, &curdir, &cmdline, Env, 0, 0, 0, 0);
 
  if (!NT_SUCCESS(status))
  {
